9e853f78f40c83352f05156d3a72626773b09769
mpv <ariel@gcoop.coop>
2016-06-26 10:54:31 -0300

    Add state parameter to firefox_profile modul

--- library/firefox_profile.py
+++ library/firefox_profile.py
@@ -1,29 +1,93 @@
 #!/usr/bin/python
 
 from ansible.module_utils.basic import *
-import glob
+from collections import OrderedDict
 import os.path
 import subprocess
+import ConfigParser
+import shutil
 
-def profile_path(name, path):
-    path = os.path.expanduser(path)
-    profile = glob.glob(os.path.join(path, '*.%s' % name))
-    if profile:
-        return profile[0]
-    raise Exception('Profile %s not found' % name)
+class FirefoxConfigWrapper:
+    """Wrapper around file object to remove spaces around .ini file delimiters.
 
-def profile_exists(name, path):
-    path = os.path.expanduser(path)
-    if glob.glob(os.path.join(path, '*.%s' % name)):
-        return True
-    return False
+    Taken from http://stackoverflow.com/a/25084055.
+    """
 
-def create_profile(name, path):
-    command = 'firefox -no-remote -CreateProfile %s' % name
-    p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    (stdout, stderr) = p.communicate()
-    if p.returncode != 0 or not profile_exists(name, path):
-        raise Exception(stderr)
+    output_file = None
+    def __init__(self, new_output_file):
+        self.output_file = new_output_file
+
+    def write(self, what):
+        self.output_file.write(what.replace(" = ", "="))
+
+
+class FirefoxProfiles:
+    """Class to manage firefox profiles."""
+
+    def __init__(self, path):
+        self.path = os.path.expanduser(path)
+        self.profiles_ini = os.path.join(self.path, 'profiles.ini')
+        self.config = ConfigParser.RawConfigParser()
+        # Make options case sensitive
+        self.config.optionxform = str
+        self.read()
+
+    def read(self):
+        self.config.read(self.profiles_ini)
+        self.sections = OrderedDict()
+        for section in self.config.sections():
+            if section != 'General':
+                profile = dict(self.config.items(section))
+                self.sections[profile['Name']] = section
+
+    def write(self):
+        # Reorder the current sections, otherwise firefox deletes them on start.
+        new = ConfigParser.ConfigParser()
+        new.optionxform = str
+        new.add_section('General')
+        for item in self.config.items('General'):
+            new.set('General', item[0], item[1])
+
+        index = 0
+        for section in self.sections.values():
+            new_section = 'Profile%d' % index
+            new.add_section(new_section)
+            for item in self.config.items(section):
+                new.set(new_section, item[0], item[1])
+            index += 1
+
+        with open(self.profiles_ini, 'wb') as config_file:
+            new.write(FirefoxConfigWrapper(config_file))
+
+        # Update state with the new file.
+        self.read()
+
+    def get(self, name):
+        if name in self.sections:
+            return dict(self.config.items(self.sections[name]))
+
+    def get_path(self, name):
+        profile = self.get(name)
+        if (bool(profile['IsRelative'])):
+            return os.path.join(self.path, profile['Path'])
+        return profile['Path']
+
+    def delete(self, name):
+        profile = self.get(name)
+        if profile is not None:
+            shutil.rmtree(self.get_path(name))
+            self.sections.pop(name)
+            self.write()
+
+    def create(self, name):
+        command = 'firefox -no-remote -CreateProfile %s' % name
+        p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        (stdout, stderr) = p.communicate()
+        if p.returncode != 0:
+            raise Exception(stderr)
+        self.read()
+
+
 
 def main():
     fields = {
@@ -36,17 +100,17 @@ def main():
         },
     }
     module = AnsibleModule(argument_spec=fields)
-
-    if profile_exists(module.params['name'], module.params['path']):
-        changed = False
-    else:
-        try:
-            create_profile(module.params['name'], module.params['path'])
-            changed = True
-        except Exception as e:
-            module.fail_json(msg=e.message)
-
-    result = {'profile_path': profile_path(module.params['name'], module.params['path'])}
+    profiles = FirefoxProfiles(module.params['path'])
+    name = module.params['name']
+    changed = False
+    result = None
+    if module.params['state'] == 'present' and profiles.get(name) is None:
+        profiles.create(name)
+        changed = True
+        result = {'profile': profiles.get(name)}
+    elif module.params['state'] == 'absent' and profiles.get(name) is not None:
+        profiles.delete(name)
+        changed = True
     module.exit_json(changed=changed, meta=result)
 
 
